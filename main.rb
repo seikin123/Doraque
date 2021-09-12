8a559d65aa864ed472055062c1cc64c128942172
ヨシダタイキ <abc521fjr7@gmail.com>
2021-09-12 13:34:03 +0900

    [add]モンスタークラスのインスタンスがメソッド内で呼び出し

--- main.rb
+++ main.rb
@@ -5,25 +5,25 @@ class Brave
   # セッターゲッターを一括定義
   attr_accessor :hp
   # 引数に**を記述：ハッシュしか受け取れなくなる
-  def initialize(**params)
+  def initialize(params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
   end
 
+  def attack(monster)
+    puts "テスト：#{monster.name}のHPを表示 -> #{monster.hp}"
+  end
+
 end
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 
-brave.hp -= 30
-
-puts "#{brave.name}はダメージを受けた! 残りHPは#{brave.hp}だ"
 
 class Monster
   attr_reader :name, :offense, :defense
-
+  
   attr_accessor :hp
-
+  
   # **paramsにすることでハッシュ形式の引数しか受け付けないようにできる
   def initialize(**params)
     @name = params[:name]
@@ -31,7 +31,12 @@ class Monster
     @offense = params[:offense]
     @defense = params[:defense]
   end
-
+  
 end
 
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+
+brave.attack(monster)
+
+
