ce3811bfb0987f7c61f649bbd92af3b857e09404
ヨシダタイキ <abc521fjr7@gmail.com>
2021-09-12 14:16:59 +0900

    [通常攻撃]モンスターの攻撃処理実装まで

--- main.rb
+++ main.rb
@@ -2,10 +2,12 @@ class Brave
   # attr_readerの記述でゲッターを省略することができる
   # 複数の値を同時に指定することができる
   attr_reader :name, :offense, :defense
-  # セッターゲッターを一括定義
   attr_accessor :hp
-  # 引数に**を記述：ハッシュしか受け取れなくなる
-  def initialize(params)
+
+  # 必殺攻撃の計算に使う定数
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
+  def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
@@ -13,15 +15,26 @@ class Brave
   end
 
   def attack(monster)
-    
-    # ダメージ計算の処理を追加
-    damage = @offense - monster.defense
-    # 自己代入：monster.hpからdamageを引いた値をmonster.hpに代入
+    puts "#{@name}の攻撃"
+
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     monster.hp -= damage
 
-    # メッセージを追記
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
   end
 
 end
@@ -29,7 +42,9 @@ end
 class Monster
   attr_reader :name, :offense, :defense
   attr_accessor :hp
-  
+
+  POWER_UP_RATE = 1.5
+
   # **paramsにすることでハッシュ形式の引数しか受け付けないようにできる
   def initialize(**params)
     @name = params[:name]
@@ -38,6 +53,17 @@ class Monster
     @defense = params[:defense]
   end
   
+  def attack(brave)
+    puts "#{@name}の攻撃"
+  
+    # 勇者に与えるダメージの計算
+    damage = @offense - brave.defense
+    # 勇者クラスのHPにダメージを反映
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
