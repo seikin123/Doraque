68f95590d6ff1648a63229a5187797bc351dba0b
ヨシダタイキ <abc521fjr7@gmail.com>
2021-09-12 11:31:15 +0900

    [add]パラメーターの設定

--- main.rb
+++ main.rb
@@ -3,4 +3,25 @@ class Brave
   def name=(name)
     @name = name
   end
+
+  #hpのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  # offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+
+  # defenseのセッター
+  def defense=(defense)
+    @defense = defense
+  end
 end
+
+brave = Brave.new
+
+brave.name = "テリー"
+brave.hp = 500
+brave.offense = 150
