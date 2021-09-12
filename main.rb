45d09848d232efbea680b04ae6bf9d0872a82d2b
ヨシダタイキ <abc521fjr7@gmail.com>
2021-09-12 11:45:53 +0900

    [add]パラメーターの初期値を設定

--- main.rb
+++ main.rb
@@ -1,53 +1,54 @@
+
 class Brave
-  #セッター
-  def name=(name)
-    @name = name
+
+  def initialize
+    @name = "テリー"
+    @hp = 500
+    @offense = 150
+    @defense = 100
   end
 
-  # nameのゲッター
+  # 以下を削除
+  # def name=(name)
+  #   @name = name
+  # end
+
   def name
     @name
   end
 
-  #セッター クラス内にあるインスタンス変数の値を取得し、クラス外から参照できるようにするためのメソッド
-  def hp=(hp)
-    @hp = hp
-  end
+  # 以下を削除
+  # def hp=(hp)
+  #   @hp = hp
+  # end
 
-  # hpのゲッター
   def hp
     @hp
   end
 
-  #セッター
-  def offense=(offense)
-    @offense = offense
-  end
+  # 以下を削除
+  # def offense=(offense)
+  #   @offense = offense
+  # end
 
-  # offenseのゲッター
   def offense
     @offense
   end
 
-  #セッター
-  def defense=(defense)
-    @defense = defense
-  end
+  # 以下を削除
+  # def defense=(defense)
+  #   @defense = defense
+  # end
 
-  # defenseのゲッター
   def defense
     @defense
   end
+
 end
 
 brave = Brave.new
 
-brave.name = "テリー"
-brave.hp = 500
-brave.offense = 150
-brave.defense = 100
-
-puts <<~TEXT #ヒアドキュメントの記法
+puts <<~TEXT
 NAME：#{brave.name}
 HP：#{brave.hp}
 OFFENSE：#{brave.offense}
